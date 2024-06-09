73d3b09fb36e4885dffd724c6a890a3cc812bc53
aayush-506 <140447060+aayush-506@users.noreply.github.com>
2024-06-10 01:43:32 +0530

    pre-final

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function addDate() {
     form.addEventListener("submit", function (event) {
       event.preventDefault();
       if (task_input.value === "") {
-        alert("Please Enter the Task before pressing ADD");
+        alert("Please Enter the Task before adding");
       } else {
 
         let inline = document.createElement("div");
@@ -170,26 +170,5 @@ function addDate() {
   }
 }
 
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function () {
-//     /* Toggle between adding and removing the "active" class,
-//         to highlight the button that controls the panel */
-//     this.classList.toggle("active");
 
-//     /* Toggle between hiding and showing the active panel */
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
 
-// button.addEventListener("click", function () {
-//   let ul_pannel = document.createElement("ul");
-//   ul_pannel.classList.add("list-container");
-//   panel.appendChild(ul_pannel);
-// });
